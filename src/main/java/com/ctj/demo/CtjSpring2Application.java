f4c190104bc72beb73f9dc7def33f6a394c7ebc4
chacha86 <taejincha0619@gmail.com>
2020-01-19 11:18:00 +0900

    aa

---
+++ src/main/java/com/ctj/demo/CtjSpring2Application.java
@@ -0,0 +1,14 @@
+package com.ctj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtjSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtjSpring2Application.class, args);
+		System.out.println("aaa");
+	}
+
+}
